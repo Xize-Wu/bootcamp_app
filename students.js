6c057e340b137568746bed02a84aaad4c3ba868d
Xize Wu <wuxize1995@gmail.com>
2023-04-06 15:23:43 -0400

    use parameterized query to prevent sql injection attack

--- students.js
+++ students.js
@@ -7,16 +7,35 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+
+const text = `SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students JOIN cohorts on students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2`;
+
+
+// pool.query(`
+// SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+// FROM students JOIN cohorts on students.cohort_id = cohorts.id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`)
+//     })
+//   })
+//   .catch(err => console.error('query error', err.stack));
+
+pool
+  .query(text, values)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`)
-    })
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.`);
+    });
   })
-  .catch(err => console.error('query error', err.stack));
+  .catch(e => console.error(e.stack))
